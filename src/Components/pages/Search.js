5dc659b66851bcb09eb6500eb5294b76f70ef22c
Blake <bparker9902@gmail.com>
2023-10-28 00:53:06 -0500

    search page in progress

--- src/Components/pages/Search.js
+++ src/Components/pages/Search.js
@@ -1,4 +1,28 @@
+import "../pagesCSS/search.css";
+
 function Search() {
-  return <h1>This is a demo to show Zacttt!</h1>;
+  return (
+    <>
+      <div className="search">
+        <h1>Search For Posts</h1>
+        <form>
+          <div className="input-container">
+            <input type="text" id="search" placeholder="Search..." />
+            <input type="submit" value="Go" />
+          </div>
+          <div className="button-container">
+            <button type="button" id="button1">
+              Button 1
+            </button>
+            <div id="divider"></div>
+            <button type="button" id="button2">
+              Button 2
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
+
 export default Search;
