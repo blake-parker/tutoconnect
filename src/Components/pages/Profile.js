a9a14770af0fda00b3eba23340e5025f014363eb
Blake <bparker9902@gmail.com>
2023-10-28 19:12:50 -0500

    test logout button added to profile page

--- src/Components/pages/Profile.js
+++ src/Components/pages/Profile.js
@@ -1,10 +1,23 @@
 import Navbar from "../NavBar/Navbar";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
+  const navigate = useNavigate();
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch (e) {
+      console.error("Error signing in:", e.message);
+    }
+  };
   return (
     <>
       <Navbar />
       <h1>Logged In User Profile</h1>
+      <button onClick={handleSignOut}>log out</button>
     </>
   );
 }
