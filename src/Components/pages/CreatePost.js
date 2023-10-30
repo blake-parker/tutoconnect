3ba46a575ca96ed980ec90a7fbaa3001a9823793
Blake Parker <bparker9902@gmail.com>
2023-10-29 19:05:34 -0500

    createPost wip

--- src/Components/pages/CreatePost.js
+++ src/Components/pages/CreatePost.js
@@ -1,4 +1,28 @@
+import "../pagesCSS/createPost.css";
+
 function CreatePost() {
-  return <div>CreatePost</div>;
+  return (
+    <div className="createPostPage">
+      <div className="cpContainer">
+        <h1>Create A Post</h1>
+        <div className="inputGp">
+          <label> Title:</label>
+          <input placeholder="Title..." />
+        </div>
+        <div className="inputGp">
+          <label> Post:</label>
+          <textarea placeholder="Post..." />
+        </div>
+        <label>
+          I am a...
+          <select name="selectedStatus" defaultValue="student">
+            <option value="student">Student</option>
+            <option value="tutor">Tutor</option>
+          </select>
+        </label>
+        <button> Submit Post</button>
+      </div>
+    </div>
+  );
 }
 export default CreatePost;
--- src/Components/pages/Posts.js
+++ src/Components/pages/Posts.js
@@ -1,4 +1,29 @@
+import { getDocs, collection } from "firebase/firestore";
+import { auth, db } from "../firebase";
+import { useState, useEffect } from "react";
+
 function Posts() {
-  return <div>Posts</div>;
+  const [posts, setPosts] = useState([]);
+
+  const fetchPost = async () => {
+    await getDocs(collection(db, "studentPosts")).then((querySnapshot) => {
+      const newData = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setPosts(newData);
+      console.log(posts, newData);
+    });
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  return (
+    <>
+      <div></div>
+    </>
+  );
 }
 export default Posts;
