39f92b17716496ac1670429213bf6e9a38595228
Zach <zyoung7@lsu.edu>
2023-11-01 20:34:43 -0500

    added css import

--- src/Components/pages/SignUp.jsx
+++ src/Components/pages/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
+import "../CSS/SignUp.css";
 
 function SignUp() {
   const [email, setEmail] = useState('');
