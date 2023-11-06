a67398b380049198f6ccf7db7d3b219f89bee9f7
Blake Parker <bparker9902@gmail.com>
2023-11-06 15:47:45 -0600

    full username functionality added

--- src/Components/pages/SignUp.jsx
+++ src/Components/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
 import "../CSS/SignUp.css";
@@ -8,11 +8,17 @@ import "../CSS/SignUp.css";
 function SignUp() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
   const navigate = useNavigate();
 
   const handleSignUp = async () => {
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
+      const userCred = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCred.user;
+
+      await updateProfile(user, {
+        displayName: username
+      });
       navigate('/');
     } catch (error) {
       console.error('Error signing up:', error.message);
@@ -23,14 +29,21 @@ function SignUp() {
     <div className="body">
     <div className="profile-setup">
       <h2>Profile Set Up</h2>
-      <label>username</label>
+      <label>Username</label>
+      <input
+        type="username"
+        placeholder=""
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <label >Email</label>
       <input
         type="email"
         placeholder=""
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
-      <label>password</label>
+      <label>Password</label>
       <input
         type="password"
         placeholder=""
@@ -39,17 +52,17 @@ function SignUp() {
       />
        <label>What School Do You Attend?
 
-<select>
-  
-  <option value="Lousiana State university">Lousiana State University</option>
+        <select>
+          
+          <option value="Lousiana State university">Lousiana State University</option>
 
-  <option value="Texas A&M">Texas A&M</option>
+          <option value="Texas A&M">Texas A&M</option>
 
-  <option value="'Bama :(">Bama</option>
+          <option value="'Bama :(">Bama</option>
 
-</select>
+        </select>
 
-</label>
+      </label>
       <button className="submit-btn" onClick={handleSignUp}>Get Started!</button>
       <p>
         Already have an account? <a href="/login">Log In</a>
