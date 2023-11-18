eb5ef6dc2d8fcd37a1bddf77994b4bd28f0c7814
Blake <bparker9902@gmail.com>
2023-11-18 16:14:12 -0600

    landing page finished

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,10 +1,27 @@
 import Navbar from "../NavBar/Navbar.jsx";
+import { auth } from "../firebase.jsx";
+import logo from "../../logo/small_logo.png";
+import "../CSS/Landing.css";
+import { useNavigate } from "react-router";
 
 function Home() {
+  const nav = useNavigate();
+  const handleNav = () => {
+    nav("/search");
+  };
   return (
     <>
       <Navbar />
-      <h1>Home Page</h1>
+      <div className="lp-container">
+        <div className="picture">
+          <img src={logo} alt="" />
+        </div>
+        <div className="welcome">
+          <h1>Welcome to tutoConnect, {auth.currentUser.displayName}!</h1>
+          <h2>Discover the perfect tutor for your needs now</h2>
+          <button onClick={handleNav}>Get Started</button>
+        </div>
+      </div>
     </>
   );
 }
