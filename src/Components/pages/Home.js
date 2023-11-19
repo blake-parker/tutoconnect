db13f4c4dd918bcb7b79cf8aed9216e9115bf3b5
Blake Parker <bparker9902@gmail.com>
2023-11-19 13:04:06 -0600

    added random color to username landing

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -3,12 +3,32 @@ import { auth } from "../firebase.jsx";
 import logo from "../../logo/small_logo.png";
 import "../CSS/Landing.css";
 import { useNavigate } from "react-router";
+import { useEffect, useState } from "react";
 
 function Home() {
   const nav = useNavigate();
+  const [random, setRandom] = useState(0);
   const handleNav = () => {
     nav("/search");
   };
+  const colors = [
+    "#ff5733",
+    "#8a2be2",
+    "#00ff00",
+    "#ff1493",
+    "#4169e1",
+    "#ffff00",
+    "#32cd32",
+    "#ff4500",
+    "#a020f0",
+    "#00ced1",
+  ];
+
+  useEffect(() => {
+    const newRandomNumber = Math.floor(Math.random() * colors.length - 1) + 1;
+    setRandom(newRandomNumber);
+  });
+
   return (
     <>
       <Navbar userPhotoURL={auth.currentUser.photoURL} />
@@ -17,7 +37,13 @@ function Home() {
           <img src={logo} alt="" />
         </div>
         <div className="welcome">
-          <h1>Welcome to tutoConnect, {auth.currentUser.displayName}!</h1>
+          <h1>
+            Welcome to tutoConnect,{" "}
+            <p style={{ color: colors[random] }}>
+              &nbsp;{auth.currentUser.displayName}
+            </p>
+            !
+          </h1>
           <h2>Discover the perfect tutor for your needs now</h2>
           <button onClick={handleNav}>Get Started</button>
         </div>
